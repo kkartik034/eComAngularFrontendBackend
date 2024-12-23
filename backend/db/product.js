5ea41d316e315d8f0488a1241265020e01a8f821
kkartik034 <kkartik034@gmail.com>
2024-12-23 08:16:49 +0530

    added product form and added funtionality of add and update , also created some ui and all the component and functionality works great

--- backend/db/product.js
+++ backend/db/product.js
@@ -4,10 +4,11 @@ const productSchema= new mongoose.Schema({
     name: String,
     shotDescription: String,
     discription : String,
-    Price: Number,
+    price: Number,
     discount: Number,
     images:Array(String),
-    categoryId: {type:Schema.Types.ObjectId, ref:'categories'}
+    categoryId: {type:Schema.Types.ObjectId, ref:'categories'},
+    brandId: {type:Schema.Types.ObjectId, ref:'brands'}
 });
 const Product = mongoose.model("product",productSchema);
 module.exports = Product; 
